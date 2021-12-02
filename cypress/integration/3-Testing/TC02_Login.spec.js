0d3136f69b8f6b29bb86a90c95e5dc659c1a1601
devipuspitaaa <puspitasaridevi228@gmail.com>
2021-12-02 20:42:32 +0700

    Testing LOGIN

---
+++ cypress/integration/3-Testing/TC02_Login.spec.js
@@ -0,0 +1,31 @@
+describe("Refresh Database", ()=>{
+    beforeEach(()=>{
+        cy.refreshDatabase();
+        cy.seed('UsersSeeder');
+        cy.seed('RuangSeeder');
+        cy.seed('PaketSeeder');
+        cy.seed('KehadiranSeeder');
+        cy.seed('HariSeeder');
+    });
+
+    it('database succesfully refresh', () => {
+        // php artisan migrate:fresh has been
+        // called at this point
+    });
+
+
+    it('loginadmin', () => {
+        cy.fixture("admin").then(function(admin){
+            this.admin = admin;
+            cy.visit("/login");
+            cy.url().should('include','/login');
+            cy.get("#email").type(this.admin.email);
+            cy.get("#password").type(this.admin.password);
+            cy.get("#btn-login").click();
+            cy.contains("Dashboard").should("be.visible");
+
+        });
+        
+    });
+    
+});
